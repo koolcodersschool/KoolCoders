c427f83c4e1ef051b19d92ae28f87e2f191c3728
nijat1985 <57869658+nijat1985@users.noreply.github.com>
2021-11-20 01:32:42 -0500

    Update LoginTest.java

--- src/day6_multi_branch_if_statements_and_nested_if_statement/lesson2/LoginTest.java
+++ src/day6_multi_branch_if_statements_and_nested_if_statement/lesson2/LoginTest.java
@@ -2,16 +2,16 @@ package day6_multi_branch_if_statements_and_nested_if_statement.lesson2;
 
 public class LoginTest {
     public static void main(String[] args) {
-        String username = "dbtuser";
+        String username = "koolcoders";
         String password = "abc123";
 
-        if(username.equalsIgnoreCase("cbtuser") && password.equals("abc123")){
+        if(username.equalsIgnoreCase("koolcoders") && password.equals("abc123")){
             System.out.println("Login Succesfull. Welcome");
-        }else if(!username.equalsIgnoreCase("cbtuser") && password.equals("abc123")){
+        }else if(!username.equalsIgnoreCase("koolcoders") && password.equals("abc123")){
             System.out.println("Invalid username. Try again");
-        }else if(username.equalsIgnoreCase("cbtuser") && !password.equals("abc123")){
+        }else if(username.equalsIgnoreCase("koolcoders") && !password.equals("abc123")){
             System.out.println("Invalid password. Try again");
-        }else if (!username.equalsIgnoreCase("cbtuser") && !password.equals("abc123")){
+        }else if (!username.equalsIgnoreCase("koolcoders") && !password.equals("abc123")){
             System.out.println("Invalid userrname and password");
         }
 
